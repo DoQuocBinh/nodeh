f1497830498b9afe2a6cc3d058b797d3f6229a5b
Binh Do Quoc <binhdq@fpt.edu.vn>
2020-02-18 16:31:03 +0700

    first commit

---
+++ server.js
@@ -0,0 +1,10 @@
+var http=require('http')
+var port = process.env.PORT || 3000;
+
+var server=http.createServer((function(request,response)
+{
+	response.writeHead(200,
+	{"Content-Type" : "text/plain"});
+	response.end("Hello World\n");
+}));
+server.listen(port);
